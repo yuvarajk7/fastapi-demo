ba72c263fe05b8f392e7768f9505d74e2163a71e
yuvarajk7 <yuvaraj.kesavan@hotmail.com>
2025-05-28 11:35:10 -0500

    Added versioning middleware support backward compatibility - Breaks swagger UI. To Support add path exclusions

--- app/middlewares/version.py
+++ app/middlewares/version.py
@@ -6,8 +6,21 @@ from starlette.requests import Request
 class VersioningMiddleware(BaseHTTPMiddleware):
     version_regex = re.compile(r"^/v[0-9]+/")
 
+    excluded_paths = [
+        "/docs",
+        "/redoc",
+        "/openapi.json",
+        "/swagger-ui.css",
+        "/swagger-ui-bundle.js",
+        "/static/",
+    ]
+
     async def dispatch(self, request: Request, call_next):
         path = request.url.path
+
+        # Check if path is in excluded paths
+        if any(path.startswith(excluded) for excluded in self.excluded_paths):
+            return await call_next(request)
 
         if self.version_regex.match(path):
             return await call_next(request)
